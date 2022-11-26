58caebb18e09139e12c4976c075747885d8c4a11
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-11-26 13:51:15 +0200

    Changes for prod

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const closeElem = document.querySelector(".menu__close");
+//const sectionLinks = document.querySelectorAll(".section-link")
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active");
@@ -10,9 +11,69 @@ closeElem.addEventListener("click", () => {
     menu.classList.remove("active");
 });
 
-const counters = document.querySelectorAll(".skills__counter");
-const lines = document.querySelectorAll(".skills__meter-bar span");
+// const counters = document.querySelectorAll(".skills__counter");
+// const lines = document.querySelectorAll(".skills__meter-bar span");
 
-counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
+// counters.forEach((item, i) => {
+//     lines[i].style.width = item.innerHTML;
+// });
+
+const scrolling = (selector) => {
+    const upElem = document.querySelector(selector);
+
+    window.addEventListener("scroll", () => {
+        if (document.documentElement.scrollTop > 1650) {
+            upElem.classList.add("animate__animated", "animate__fadeIn");
+            upElem.classList.remove("animate__fadeOut");
+        } else {
+            upElem.classList.add("animate__fadeOut");
+            upElem.classList.remove("animate__fadeIn");
+        }
+    });
+
+    // Scroll with requestAnimationFrame //
+
+    let links = document.querySelectorAll("[href^='#']");
+    let speed = 0.2;
+
+    links.forEach(a => {
+        a.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop;
+            let hash = this.hash;
+            let toBlock = document.querySelector(hash).getBoundingClientRect().top;
+            let start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start;
+                let r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+                document.documentElement.scrollTo(0, r);
+
+                if (r !== widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+
+            console.log("asd")
+        });
+    });
+};
+
+// sectionLinks.forEach(a => {
+//     a.addEventListener("click", () => {
+//         console.log("aaa");
+//         //scrolling(a);
+//     });
+// });
+
+scrolling(".pageup");
