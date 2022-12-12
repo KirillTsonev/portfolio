b81566cb41b265da55be51003f55c1685c1fd3aa
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-12 20:04:05 +0200

    Change

--- js/components/hamburger.js
+++ js/components/hamburger.js
@@ -6,19 +6,23 @@ function hamburger() {
 
     hamburger.addEventListener("click", () => {
         menu.classList.add("active");
+        document.body.style.overflow = "hidden";
     });
 
     closeElem.addEventListener("click", () => {
         menu.classList.remove("active");
+        document.body.style.overflow = "";
     });
 
     overlay.addEventListener("click", () => {
         menu.classList.remove("active");
+        document.body.style.overflow = "";
     });
 
     document.addEventListener("keydown", (e) => {
         if (e.code === "Escape" && menu.classList.contains("active")) {
             menu.classList.remove("active");
+            document.body.style.overflow = "";
         }
     });
 }
